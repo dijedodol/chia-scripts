ab4e384dbbd7686df256fe51f69a319c6b91da35
Gde Mahardhikha Satrigraha <gde.satrigraha@gmail.com>
2021-05-14 04:49:53 +0700

    do not rely with systemd restarting the plotter service, in case systemd decided to giveup retrying

--- chia/start-plotter.sh
+++ chia/start-plotter.sh
@@ -14,24 +14,14 @@ fi
 plots_tmp_dir="${HOME}/plots-tmp/${suffix}"
 mkdir -p "${plots_tmp_dir}"
 
-fn_start_plotter_with_file() {
+# loop whenever done plotting, unless we were told to stop
+touch "${plots_tmp_dir}.loop"
+while [ -f "${plots_tmp_dir}.loop" ]; do
   bash -c 'chia plots create -r 4 -f '"${chia_farmer_public_key}"' -p '"${chia_pool_public_key}"' -t '"${plots_tmp_dir}"' -d '"${HOME}/gv-chia/plots"' -e' &
   pid=$!
   echo "${plots_tmp_dir}" | tee "${plots_tmp_dir}.pid" > /dev/null
   wait "$pid"
   rm -f "${plots_tmp_dir}.pid"
-}
-
-# start chia plotter, on systemd, we won't loop and rely on systemd restart behavior
-. "${HOME}/chia-blockchain/activate"
-
-if [ "systemd" = "${run_type}" ]; then
-  fn_start_plotter_with_file
-else
-  touch "${plots_tmp_dir}.loop"
-  while [ -f "${plots_tmp_dir}.loop" ]; do
-    fn_start_plotter_with_file
-  done
-fi
+done
 
 rm -rf "${plots_tmp_dir}"
--- chia/stop-plotter.sh
+++ chia/stop-plotter.sh
@@ -3,20 +3,11 @@ set -v
 . constants.sh
 
 suffix="$1"
-run_type="$2"
-
 if [ -z "${suffix}" ]; then
   echo "suffix is missing"
   exit 1
 fi
 
-if [ -z "${run_type}" ]; then
-  echo "run_type is missing"
-  exit 1
-fi
-
 plots_tmp_dir="${HOME}/plots-tmp/${suffix}"
-if [ "systemd" != "${run_type}" ]; then
-  rm -f "${plots_tmp_dir}.loop"
-fi
+rm -f "${plots_tmp_dir}.loop"
 kill "$(cat "${plots_tmp_dir}.pid")"
