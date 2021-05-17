79768604b99c285d3e0db8e2a98cdb94b6267609
Gde Mahardhikha Satrigraha <gde.satrigraha@gmail.com>
2021-05-17 12:13:14 +0700

    update hpool miner version

--- aws/ec2-init/chia-hpool-miner.sh
+++ aws/ec2-init/chia-hpool-miner.sh
@@ -23,35 +23,36 @@ mkdir -p "${HOME}/gv-chia/plots"
 # chia install
 chia/install.sh
 
-# hpool install & setup systemd unit
+# hpool miner install & setup systemd unit
 mkdir -p "${HOME}/hpool"
-wget 'https://github.com/hpool-dev/chia-miner/releases/download/v1.3.0-3/HPool-Miner-chia-v1.3.0-3-aarch64.zip' -O "${HOME}/hpool/hpool-miner.zip"
+wget 'https://github.com/hpool-dev/chia-miner/releases/download/v1.3.0-4/HPool-Miner-chia-v1.3.0-4-linux.zip' -O "${HOME}/hpool/hpool-miner.zip"
 unzip -jo "${HOME}/hpool/hpool-miner.zip" -x .DS_Store -d "${HOME}/hpool"
 rm -f "${HOME}/hpool/hpool-miner.zip"
 
 # construct hpool config
 tee "${HOME}/hpool/config.yaml" > /dev/null <<EOF
 token: ""
+path:
+- ${HOME}/gv-chia/plots
+minerName: "$(cat "${HOME}/aws_region")"
 apiKey: 3df2d2c3-d437-4f4d-83c5-e096f8ceddc8
 cachePath: ""
 deviceId: ""
 extraParams: {}
 log:
   lv: info
-  path: ./log
+  path: ./log/
   name: miner.log
 url:
   info: ""
   submit: ""
   line: ""
-scanPath: true
-scanMinute: 5
+scanPath: false
+scanMinute: 15
 debug: ""
 language: en
-path:
+singleThreadLoad: false
 EOF
-echo "- ${HOME}/gv-chia/plots" | tee -a "${HOME}/hpool/config.yaml"
-echo "minerName: $(curl ifconfig.co)" | tee -a "${HOME}/hpool/config.yaml"
 
 # setup hpool miner systemd unit
 sudo cp -f 'systemd/unit/chia-hpool-miner.service' /etc/systemd/system/
