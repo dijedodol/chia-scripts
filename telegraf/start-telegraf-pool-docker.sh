835fc918714f4b2e5497d6b22c1ccf06ddbec7d6
Gde Mahardhikha Satrigraha <gde.satrigraha@gmail.com>
2021-05-18 21:39:26 +0700

    limit docker log file size

--- telegraf/start-telegraf-pool-docker.sh
+++ telegraf/start-telegraf-pool-docker.sh
@@ -2,4 +2,4 @@
 set -x
 . constants.sh
 
-sudo docker run --rm --name telegraf-pool -p '18080:18080' -p '9273:9273' --rm 'dijedodol/telegraf-pool:latest'
+sudo docker run --rm --log-driver json-file --log-opt max-size=10m --log-opt max-file=5 --name telegraf-pool -p '18080:18080' -p '9273:9273' --rm 'dijedodol/telegraf-pool:latest'
